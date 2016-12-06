e6ba28d738e75d3ba1ee61091a3044e690a1a790
howardschwartz <howardschwartz1221@gmail.com>
2016-12-06 15:24:15 -0500

    Add files via upload

---
+++ compute_critic_update.m
@@ -0,0 +1,22 @@
+function [psi] = compute_critic_update( robot, no_of_robots)
+% [psi] = compute_critic_update( robot )
+%   Compute the update to the critic parameters
+%
+   for i = 1:no_of_robots
+       reward = robot(i).reward_capture_heading;
+       gamma = robot(i).gamma_capture_heading;
+       alpha = robot(i).alpha;
+       value = robot(i).value_capture_heading;
+       value_old = robot(i).value_old_capture_heading;
+       psi = robot(i).psi;
+       phi_norm =robot(i).phi_norm_critic;
+       no_of_rules = robot(i).no_of_rules_critic;
+       td = (reward + gamma*value) - value_old;
+       for j=1:no_of_rules
+           psi(j) = psi(j)+ alpha*td*phi_norm(j);
+           %sprintf(' Value psi(%d) is %f', j, psi(j))
+       end
+       robot(i).psi = psi;
+   end
+end
+
---
+++ compute_robot_state_value.m
@@ -0,0 +1,31 @@
+function [value, phi_norm] = compute_robot_state_value( robot, input )
+% [value] = compute_robot_state_value(robot, input)
+% Computes the value of the state
+%
+    no_of_rules = robot.no_of_rules_critic;
+    rule.mf = robot.rule_critic
+    phi = zeros(1, no_of_rules); 
+    for i=1:no_of_rules
+        phi(i) = fire_strength_for_rule(input, rule.mf);
+    end
+    %
+    % Compute the normalized firing strength for each rule.
+    %
+    phi_sum = sum(phi);
+    phi_norm = zeros(1, no_of_rules);
+    if (phi_sum ~= 0)
+       for i=1:no_of_rules
+           phi_norm(i) = phi(i)/phi_sum;
+       end
+    end
+    %
+    % Compute the new value of the state
+    %
+    value = 0;
+    for i=1:no_of_rules
+        value = value + phi_norm(i)*robot.psi(i);
+    end
+
+
+end
+
