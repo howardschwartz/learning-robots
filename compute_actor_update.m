e9f75b107de42e55354258adf1ccd5c01d6dca87
Howard Schwartz <howardschwartz1221@gmail.com>
2017-03-29 12:24:01 -0400

    Add files via upload

--- compute_actor_update.m
+++ compute_actor_update.m
@@ -2,21 +2,19 @@ function [w] = compute_actor_update(robot, no_of_robots)
 % [w] = compute_actor_update(robot, no_of_robots)
 %  compute the update to the actor parameters
 %
-   for i = 1:no_of_robots
-       reward = robot(i).reward_capture_heading;
-       gamma = robot(i).gamma_capture_heading;
-       beta = robot(i).beta;
-       value = robot(i).value;
-       value_old = robot(i).value_old;
-       w = robot(i).w;
-       noise = robot(i).noise;
-       phi_norm =robot(i).phi_norm_actor;
-       no_of_rules = robot(i).no_of_rules_actor;
-       td = (reward + gamma*value) - value_old;
-       for j=1:no_of_rules
-         deltaw = beta*sign(td*noise)*phi_norm(j);
-         w(j) = w(j)+ deltaw;
-       end
-   end
+    reward = robot.reward_capture_heading;
+    gamma = robot.gamma_capture_heading;
+    beta = robot.beta;
+    value = robot.value;
+    value_old = robot.value_old;
+    w = robot.w;
+    noise = robot.noise;
+    phi_norm =robot.phi_norm_actor;
+    no_of_rules = robot.no_of_rules_actor;
+    td = (reward + gamma*value) - value_old;
+    for j=1:no_of_rules
+       deltaw = beta*sign(td*noise)*phi_norm(j);
+       w(j) = w(j)+ deltaw;
+    end
 end
 
--- compute_critic_update.m
+++ compute_critic_update.m
@@ -1,21 +1,19 @@
-function [psi] = compute_critic_update( robot, no_of_robots)
+function [psi] = compute_critic_update(robot)
 % [psi] = compute_critic_update( robot )
 %   Compute the update to the critic parameters
 %
-   for i = 1:no_of_robots
-       reward = robot(i).reward_capture_heading;
-       gamma = robot(i).gamma_capture_heading;
-       alpha = robot(i).alpha;
-       value = robot(i).value;
-       value_old = robot(i).value_old;
-       psi = robot(i).psi;
-       phi_norm =robot(i).phi_norm_critic;
-       no_of_rules = robot(i).no_of_rules_critic;
-       td = (reward + gamma*value) - value_old;
-       for j=1:no_of_rules
-           psi(j) = psi(j)+ alpha*td*phi_norm(j);
-           %sprintf(' Value psi(%d) is %f', j, psi(j))
-       end
-   end
+    reward = robot.reward_capture_heading;
+    gamma = robot.gamma_capture_heading;
+    alpha = robot.alpha;
+    value = robot.value;
+    value_old = robot.value_old;
+    psi = robot.psi;
+    phi_norm =robot.phi_norm_critic;
+    no_of_rules = robot.no_of_rules_critic;
+    td = (reward + gamma*value) - value_old;
+    for j=1:no_of_rules
+        psi(j) = psi(j)+ alpha*td*phi_norm(j);
+        %sprintf(' Value psi(%d) is %f', j, psi(j))
+    end
 end
 
