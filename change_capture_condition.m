b3524d1e3ebe8c64c9598b3cc98bdf30a5f432bd
Howard Schwartz <howardschwartz1221@gmail.com>
2017-03-29 22:07:38 -0400

    Add files via upload

--- change_capture_condition.m
+++ change_capture_condition.m
@@ -3,8 +3,8 @@ function [psi, w, sigma] = change_capture_condition(capture)
 %  This function will determine what to do when a robot can no longer
 %  capture an evader.    
    if (capture.count_success_times > 0)
-       %psi = capture.psi_success_init;
-       psi = capture.psi;
+       psi = capture.psi_success_init;
+       %psi = capture.psi;
        w = capture.w_success_init;
        sigma = capture.sigma_success;
    else
